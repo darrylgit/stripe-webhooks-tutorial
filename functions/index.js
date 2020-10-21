3e2fe5c2e9cb14e86a50c3fe4998497d4bc56cd2
darrylgit <darrylsmithup@gmail.com>
2020-10-21 09:51:26 -0400

    Verify events

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const stripe = require('stripe')(functions.config().keys.webhooks);
+const endpointSecret = functions.config().keys.signing;
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -8,6 +10,21 @@ const functions = require('firebase-functions');
 //   response.send("Hello from Firebase!");
 // });
 
+// Get the signature from the request header
+
 exports.events = functions.https.onRequest((request, response) => {
+  let sig = request.headers['stripe-signature'];
+
+  // Verify the request against our endpointSecret
+  try {
+    let event = stripe.webhooks.constructEvent(
+      request.rawBody,
+      sig,
+      endpointSecret
+    );
+  } catch (err) {
+    return response.status(400).end();
+  }
+
   response.send('Endpoint for Stripe Webhooks!');
 });
